80e7e4c849a78e0044ac41bc75988b48c0b629a8
DisTans <m.lubachenko@gmail.com>
2021-08-06 20:15:46 +0300

    again

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
-
 const app = express();
-app.use(cors({
-    origin: 'https://dazzling-noether-69eedd.netlify.app/'
-}));
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 const connection = mysql.createConnection({
@@ -15,25 +17,19 @@ const connection = mysql.createConnection({
   database: "heroku_c0ee8d059847f3c",
 });
 
-
 // app.all("/", function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "X-Requested-With");
 //   next();
 // });
 
-app.get("/", (req,res)=>{
-    connection.query(
-        'SELECT * FROM `items`',
-        function (err, results, fields) {
-          res.send(results);
-          console.log(results); // results contains rows returned by server
-          console.log(fields); // fields contains extra meta data about results, if available
-        }
-      );
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM `items`", function (err, results, fields) {
+    res.send(results);
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  });
 });
-
-
 
 const PORT = process.env.PORT || 3001;
 
