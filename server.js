610c0c15776e59a5eac23d53fd45475eb55e47ab
DisTans <m.lubachenko@gmail.com>
2021-08-06 20:04:05 +0300

    cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mysql = require("mysql2");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
-// app.use(cors());
+app.use(cors({
+    origin: 'https://dazzling-noether-69eedd.netlify.app/'
+}));
 app.use(express.json());
 
 const connection = mysql.createConnection({
